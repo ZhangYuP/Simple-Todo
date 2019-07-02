ea8f53432a41319bd9bf7cfd57793052acc45c0e
ZhangYuP <zyp5318@qq.com>
2019-07-02 22:57:23 +0800

    第二次提交

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
+import $ from 'jquery';
+import foo from './foo';
 
 function component () {
-  var element = document.createElement('div');
+  var element = $('<div></div>');
 
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
+  element.html(_.join(['Hello','webpack'], ' '));
+  
+  return element.get(0);
 }
 
 document.body.appendChild(component());
+console.log(foo)
+console.log(foo())
