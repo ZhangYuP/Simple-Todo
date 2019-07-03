8c8512d4aede488d7168e539fce45b5deaec1c8c
ZhangYuP <zyp5318@qq.com>
2019-07-03 22:28:01 +0800

    第一次 build

---
+++ build/precache-manifest.496af74bc21bb2a5eab162f04bda1f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b39f66600ecd870ea3041909c01d02",
+    "url": "/project-react-1/build/index.html"
+  },
+  {
+    "revision": "bcb7dfd392882559f07e",
+    "url": "/project-react-1/build/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "47e3c89efb720cff887b",
+    "url": "/project-react-1/build/static/css/main.926f1478.chunk.css"
+  },
+  {
+    "revision": "bcb7dfd392882559f07e",
+    "url": "/project-react-1/build/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "47e3c89efb720cff887b",
+    "url": "/project-react-1/build/static/js/main.f46a5e00.chunk.js"
+  },
+  {
+    "revision": "23daae9089ed0a2875f5",
+    "url": "/project-react-1/build/static/js/runtime~main.a496f36a.js"
+  }
+]);
