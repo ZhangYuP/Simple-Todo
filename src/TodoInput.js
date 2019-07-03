73b3680aaba982b3802a24ef60f5db0a2a46afc2
ZhangYuP <zyp5318@qq.com>
2019-07-03 21:44:07 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,7 +2,14 @@ import React,{ Component } from 'react';
 
 class TodoInput extends Component{
   render(){
-    return <input type="text" defaultValue={this.props.content} />
+    return (
+      <input type="text" defaultValue={this.props.content} onKeyPress={this.submit} />
+    )
+  }
+  submit(event){
+    if(event.key === 'Enter'){
+      console.log('用户按回车了')
+    }
   }
 }
 
