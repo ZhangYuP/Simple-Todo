ff5947d75444a28a759de729deba43270bf58558
ZhangYuP <zyp5318@qq.com>
2019-07-05 22:21:18 +0800

    拷贝 leancloud 初始化代码

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import 'normalize.css';
 import './reset.css';
 import './App.css';
 
+var AV = require('leancloud-storage');
+
+var APP_ID = 'cYc0s0wraLNbPuKaAmN2smLT-gzGzoHsz';
+var APP_KEY = '20F8YLbSIRQiopXIScbF6apv';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 class App extends Component{
   constructor(props){
     super(props)
