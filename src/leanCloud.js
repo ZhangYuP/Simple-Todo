d5a34fa34aaaf4ec5fccdb974fec102c0a3aad83
ZhangYuP <zyp5318@qq.com>
2019-07-05 22:32:03 +0800

    新建 leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,11 @@
+var AV = require('leancloud-storage');
+
+var APP_ID = 'cYc0s0wraLNbPuKaAmN2smLT-gzGzoHsz';
+var APP_KEY = '20F8YLbSIRQiopXIScbF6apv';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
