ff7e13a9983e76bc4f984e5d90688489daa8af5b
ZhangYuP <zyp5318@qq.com>
2019-07-14 22:55:02 +0800

    将登出按钮改成图标

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component{
     return (
       <div className="App">
         <h1>{this.state.user.username || '我'}的待办
-          {this.state.user.id ? <button title="登出" onClick={this.signOut.bind(this)}>登出</button> : null}
+          {this.state.user.id ? <span className="iconfont icon-logout" title="登出" onClick={this.signOut.bind(this)}></span> : null}
         </h1>
         <TodoInput content={this.state.newTodo} 
           onChange={this.changeTitle.bind(this)}
